a7540a63f6db12fb116a22d67aac547429cbbd55
ldhpm2 <53938137+ldhpm2@users.noreply.github.com>
2026-03-22 21:26:29 +0700

    Update authorInfo.ts

--- authorInfo.ts
+++ authorInfo.ts
@@ -1,6 +1,6 @@
 // Thông tin Tác giả Ứng dụng
 export const AUTHOR_INFO = {
-    name: 'Lương Đình Hùng,
+    name: 'Lương Đình Hùng',
     school: 'Trường THPT số 2 Phù Mỹ',
     address: 'Số 50 Võ Văn Dũng, xã Bình Dương, Tỉnh Gia Lai',
     role: 'Giáo viên',
